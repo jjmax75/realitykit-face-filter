d64aee5d81e452788bcc067a8956060e9410b78c
John Behan <behanj@gmail.com>
2024-01-09 18:16:11 +0000

    initial ui setup

--- facefilter/ContentView.swift
+++ facefilter/ContentView.swift
@@ -8,17 +8,54 @@
 import SwiftUI
 import RealityKit
 
+var arView: ARView!
+
 struct ContentView : View {
+    @State var propId: Int = 0
+    
     var body: some View {
-        ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            ARViewContainer(propId: $propId).edgesIgnoringSafeArea(.all)
+            
+            HStack {
+                Spacer()
+                Button(action: {
+                    self.propId = self.propId <= 0 ? 0 : self.propId - 1
+                }) {
+                    Image("PreviousButton").clipShape(Circle())
+                }
+                Spacer()
+                Button(action: {
+                    self.TakeSnapshot()
+                }) {
+                    Image("ShutterButton").clipShape(Circle())
+                }
+                Spacer()
+                Button(action: {
+                    self.propId = self.propId >= 2 ? 2 : self.propId + 1
+                }) {
+                    Image("NextButton").clipShape(Circle())
+                }
+                Spacer()
+            }
+        }
+    }
+    
+    func TakeSnapshot() {
+        arView.snapshot(saveToHDR: false) { (image) in
+            let compressedImage = UIImage(data: (image?.pngData())!)
+            
+            UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
+    @Binding var propId: Int
     
     func makeUIView(context: Context) -> ARView {
         
-        let arView = ARView(frame: .zero)
+        arView = ARView(frame: .zero)
         
         // Load the "Box" scene from the "Experience" Reality File
         let boxAnchor = try! Experience.loadBox()
